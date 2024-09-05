5f6e067342e43ca1a154f566987d4839103722a0
Carlos Valadão <61719667+CarlosValadao@users.noreply.github.com>
2024-09-05 07:57:57 -0300

    nxt conn

---
+++ nxt_con.py
@@ -0,0 +1,15 @@
+import nxt.locator
+from nxt.bluesock import BlueSock
+
+def connect_to_nxt():
+    # Substitua '00:16:53:XX:XX:XX' pelo endereço Bluetooth do seu NXT Brick
+    btsock = BlueSock('00:16:53:XX:XX:XX')  
+    if btsock:
+        nxt_brick = btsock.connect()
+        print("Conectado ao NXT Brick via Bluetooth!")
+        return nxt_brick
+    else:
+        print("Não foi possível encontrar o NXT Brick.")
+        return None
+
+nxt_brick = connect_to_nxt()

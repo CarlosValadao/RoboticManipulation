204d08003d61cdfe34519a667d39e66d3b038841
CarlosValadao <cvaladao@ecomp.uefs.br>
2024-09-24 14:41:04 -0300

    add connection features

--- modulo_supervisor/nxt_con.py
+++ modulo_supervisor/nxt_con.py
@@ -1,17 +1,22 @@
 import nxt
 import nxt.locator
+import nxt.backend.bluetooth as bluetooth
+import nxt.brick
 
 # ENDERECO MAC DO NXT 01 (COM DISPLAY BOM E EMITINDO SONS MANEIROS)
 NXT_MAC_ADDRESS = "00:16:53:09:81:69"
+MESSAGE = b'OI BLUETOOTH :)'
 
-with nxt.locator.find() as b:
-    # Get the motor connected to the port A.
-    mymotor = b.get_motor(nxt.motor.Port.A)
-    # Full circle in one direction.
-    mymotor.turn(25, 360)
-    # Full circle in the opposite direction.
-    mymotor.turn(-25, 360)
-    print(b.get_device_info())
-    print(b.get_firmware_version())
-    b.play_sound_file(False, "avemaria.mp3")
-    print(b.get_battery_level())
+nxt_brick = nxt.locator.find(host=NXT_MAC_ADDRESS)
+# Get the motor connected to the port A.
+mymotor = nxt_brick.get_motor(nxt.motor.Port.A)
+# Full circle in one direction.
+mymotor.turn(25, 360)
+# Full circle in the opposite direction.
+mymotor.turn(-25, 360)
+#nxt_brick.message_write(inbox=1, message=MESSAGE)
+#message = nxt_brick.message_read(1, True)
+#print(f'MENSAGEM LIDA DO MAILBOX 1 DO NXT {message}')
+print(nxt_brick.get_device_info())
+print(nxt_brick.get_firmware_version())
+nxt_brick.close()

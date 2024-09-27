9da07412e503d1272bf40a90220044be36a07329
CarlosValadao <cvaladao@ecomp.uefs.br>
2024-09-27 14:18:29 -0300

    fix bluetooth communication

--- modulo_supervisor/nxt_con.py
+++ modulo_supervisor/nxt_con.py
@@ -2,10 +2,11 @@ import nxt
 import nxt.locator
 import nxt.backend.bluetooth as bluetooth
 import nxt.brick
+from time import sleep
 
 # ENDERECO MAC DO NXT 01 (COM DISPLAY BOM E EMITINDO SONS MANEIROS)
 NXT_MAC_ADDRESS = "00:16:53:09:81:69"
-MESSAGE = b'OI BLUETOOTH :)'
+MESSAGE = b'Carlos Valadao'
 
 nxt_brick = nxt.locator.find(host=NXT_MAC_ADDRESS)
 # Get the motor connected to the port A.
@@ -15,6 +16,9 @@ mymotor.turn(25, 360)
 # Full circle in the opposite direction.
 mymotor.turn(-25, 360)
 nxt_brick.message_write(1, MESSAGE)
+sleep(2)
+(inbox, message) = nxt_brick.message_read(10, 0, True)
+print(f'INBOX -> {inbox}; MESSAGE -> {message}')
 #nxt_brick.message_write(inbox=1, message=MESSAGE)
 #message = nxt_brick.message_read(1, True)
 #print(f'MENSAGEM LIDA DO MAILBOX 1 DO NXT {message}')

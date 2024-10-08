179cabe8bb299b2590d6b0317d5d4f2a50468e17
CarlosValadao <cvaladao@ecomp.uefs.br>
2024-10-08 09:48:44 -0300

    new features

--- modulo_supervisor/nxt_con.py
+++ modulo_supervisor/nxt_con.py
@@ -6,7 +6,8 @@ from time import sleep
 
 # ENDERECO MAC DO NXT 01 (COM DISPLAY BOM E EMITINDO SONS MANEIROS)
 NXT_MAC_ADDRESS = "00:16:53:09:81:69"
-MESSAGE = b'vaemfrente'
+MESSAGE = b'Ni'
+MAILBOX10 = 9
 
 nxt_brick = nxt.locator.find(host=NXT_MAC_ADDRESS)
 # Get the motor connected to the port A.
@@ -15,13 +16,13 @@ nxt_brick = nxt.locator.find(host=NXT_MAC_ADDRESS)
 #mymotor.turn(25, 360)
 # Full circle in the opposite direction.
 #mymotor.turn(-25, 360)
-nxt_brick.message_write(1, MESSAGE)
+nxt_brick.message_write(0, MESSAGE)
 sleep(2)
 #(inbox, message) = nxt_brick.message_read(10, 0, True)
 #print(f'INBOX -> {inbox}; MESSAGE -> {message}')
 #nxt_brick.message_write(inbox=1, message=MESSAGE)
-message = nxt_brick.message_read(1, True)
-print(f'MENSAGEM LIDA DO MAILBOX 1 DO NXT {message}')
+(inbox, message) = nxt_brick.message_read(10, 0, True)
+print(f'MENSAGEM LIDA DO MAILBOX 1 DO NXT "{message.decode('utf-8')}"')
 print(nxt_brick.get_device_info())
 print(nxt_brick.get_firmware_version())
 nxt_brick.play_tone_and_wait(2500, 1000)
